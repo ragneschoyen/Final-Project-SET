7a683afddd0ba1d448fc215c53352bd78e3fe36b
ragneschoyen <ragne01@hotmail.no>
2024-06-05 17:35:50 +0200

    lok

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import itertools
+import pygame
+import os
+
 
 # Class representing a card in the SET game
 class SetCards:
     # Initialize the card with four properties: number, symbol, color, and shading
-    def __init__(self, number, symbol, color, shading):
-        self.number = number # 1, 2, or 3
-        self.symbol = symbol # ⋄, ∼, ◦
+    def __init__(self, color, symbol, shading, number, image_name):
         self.color = color # red, green, or purple
+        self.symbol = symbol # ⋄, ∼, ◦
         self.shading = shading # solid, striped, or open
+        self.number = number # 1, 2, or 3
+        self.image_name = image_name # to link card to image
 
     def __repr__(self): 
         # Return a string representation of the card
-        return f"Card({self.number}, {self.symbol}, {self.color}, {self.shading})"
+        return f"Card({self.color}, {self.symbol}, {self.shading}, {self.number})"
     
     def __eq__(self, other):
         # Define equality comparison between two cards based on their properties
-        return (self.number == other.number and
+        return (self.color == other.color and
                 self.symbol == other.symbol and
-                self.color == other.color and     #KAN EVT DROPPAS ????????
-                self.shading == other.shading)
+                self.shading == other.shading and     #KAN EVT DROPPAS ????????
+                self.number == other.number)
     
 
 # Function for checking if three cards is a set 
 def is_set(card_1, card_2, card_3):
-    for attribute in ['number', 'symbol', 'color', 'shading']:
+    for attribute in ['color', 'symbol', 'shading', 'number']:
         value_1 = getattr(card_1, attribute)
         value_2 = getattr(card_2, attribute)
         value_3 = getattr(card_3, attribute)
@@ -56,6 +60,31 @@ def find_one_set(cards):
             return (card_1, card_2, card_3) 
     
     return None
+
+def create_all_cards():
+    colors = ['red', 'green', 'purple']
+    shapes = ['oval', 'diamond', 'squiggle']
+    shadings = ['filled', 'shaded', 'empty']
+    numbers = [1, 2, 3]
+
+    combinations = list(itertools.product(colors, shapes, shadings, numbers))
+    image_names = [f"{color}{shape}{shade}{number}.gif" for color, shape, shade, number in combinations]
+    return image_names
+
+image_names = create_all_cards()
+for name in image_names:
+    print(name)
+
+
+
+   
+
+
+    
+
+
+
+
 
 
 
